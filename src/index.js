ffcdc5a116b7d39498108517c0d45d8afe60c68f
Klim Savanovich <Klimsava96@mail.ru>
2019-09-25 12:33:59 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arrStr = str.split("");
+  for (let i = 0; i < arrStr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arrStr[i] === bracketsConfig[j][0] && arrStr[i + 1] === bracketsConfig[j][1]) {
+        arrStr.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+  if (arrStr.length === 0) {
+    return true;
+  } else {
+  return false;
+  }
 }
